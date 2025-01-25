c9727810397d2b2fd3da7609002cfb6c6dbfbdb2
Jordon Venner <jordonvenner@usf.edu>
2025-01-25 16:19:44 -0500

    Task 1 - Store Inventory.

--- cc_2.js
+++ cc_2.js
@@ -1,1 +1,14 @@
 console.log('cc_2.js is successfully linked to index.html!')
+
+
+//Task 1 working with arrays
+
+
+let products = ['Oranges', 'Bananas', 'Rice', 'Cake', 'Cereal'];
+console.log('Initial product list:',products)
+
+products.push('flour');
+console.log('After adding a new product:',products)
+
+products.pop()
+console.log('After removing the last product:', products)
